a8f80746fc0887a21a9f6f16ac64a0cf5771ca0c
georgespearing <spearingg@gmail.com>
2020-07-07 19:59:22 -0400

    update README

---
+++ README.m
@@ -0,0 +1,5 @@
+#AERO Boards for 2020 - 2021 school year
+
+See AERO_KiCAD_Library for custom libraries
+
+
--- readme.m
+++
@@ -1,3 +0,0 @@
-AERO Boards for 2020 - 2021 school year. See AERO_KiCAD_Library for custom libraries
-
-
