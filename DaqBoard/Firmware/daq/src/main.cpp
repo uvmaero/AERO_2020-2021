43ba85920ec654e4b71651015060483e1911d9fd
georgespearing <spearingg@gmail.com>
2021-01-11 11:36:52 -0500

    daq board firmware

---
+++ DaqBoard/Firmware/daq/src/main.cpp
@@ -0,0 +1,25 @@
+/**
+ * Copyright 2020, George Spearing, UVM AERO
+ * Da 
+ */
+
+
+#include <Arduino.h>
+#include <mcp_can.h>
+
+// wheel speed sampling
+
+// damper position sampling
+
+// brake light trigger
+
+// fan trigger
+// includes auto speed sensing and pwm signal 
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
