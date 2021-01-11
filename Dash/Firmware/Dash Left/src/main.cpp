64365d27c10789ffc260bcc1938d6caca069221a
georgespearing <spearingg@gmail.com>
2021-01-11 12:00:32 -0500

    Dash Left firmware first commit

---
+++ Dash/Firmware/Dash Left/src/main.cpp
@@ -0,0 +1,24 @@
+/**
+ * Copyright 2020, George Spearing, UVM AERO
+ * Dash Left 
+ * Temperature LEDs
+ * Battery SOC LED
+ * LCD Screen
+ */
+
+#include <Arduino.h>
+#include <mcp_can.h>
+
+// Temperature Sampling
+
+// Battery SOC Calculation
+
+// LCD Screen output
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
