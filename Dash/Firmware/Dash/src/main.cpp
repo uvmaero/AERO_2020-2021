f61598ea80d0b8ec87f52411c9f169a740b6e3a2
georgespearing <spearingg@gmail.com>
2021-01-11 11:54:57 -0500

    Dash Right first commit

---
+++ Dash/Firmware/Dash/src/main.cpp
@@ -0,0 +1,24 @@
+/**
+ * Copyright 2020, George Spearing, UVM AERO
+ * Dash Right
+ * Fault LEDs, Start button, cooling switch, direction
+ * coast / regen pots for rinehart
+ * Precharge
+ */
+
+#include <Arduino.h>
+#include <mcp_can.h>
+
+// send values to rinehart (direction, coast/regen)
+
+// send cooling switch for fan
+
+// precharge
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
