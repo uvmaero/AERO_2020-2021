34dcb9fdd0ff037fe3643b5097e496316d72de30
georgespearing <spearingg@gmail.com>
2021-01-11 11:43:08 -0500

    pedalboard initial firmware commit

---
+++ PedalBoard/Firmware/PedalBoard/src/main.cpp
@@ -0,0 +1,22 @@
+/**
+ * Copyright 2020, George Spearing, UVM AERO
+ * PedalBoard
+ * Brake Pressure Sensor, Accelerator Position 
+ */
+
+#include <Arduino.h>
+#include <mcp_can.h>
+
+// accerlator pedal sampling and comapring
+
+// brake pressure sampling and comparing
+
+// send rinehart command for torque
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
