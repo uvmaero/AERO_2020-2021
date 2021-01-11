d22b44631d914ce1fe02ef51b88d651b557a1e35
georgespearing <spearingg@gmail.com>
2021-01-11 11:48:57 -0500

    Fault Latcher Firmware Init

---
+++ FaultLatcher/Firmware/FaultLatcher/src/main.cpp
@@ -0,0 +1,21 @@
+/** 
+ * Copyright 2020, George Spearing, UVM AERO
+ * Fault Latcher
+ * Get faults from TMS 1 & 2, BMS, IMD
+ * Clear with push buttons, send values over CAN
+ */
+
+#include <Arduino.h>
+#include <mcp_can.h>
+
+// Check faults
+
+// Send value over CAN
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
