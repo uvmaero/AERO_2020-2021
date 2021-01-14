69a99aa718e781b21874e6ed2b04af1dc2edb2e1
georgespearing <spearingg@gmail.com>
2021-01-14 13:46:09 -0500

    Fault Latcher Code Creation

--- FaultLatcher/Firmware/FaultLatcher/src/main.cpp
+++ FaultLatcher/Firmware/FaultLatcher/src/main.cpp
@@ -8,14 +8,73 @@
 #include <Arduino.h>
 #include <mcp_can.h>
 
-// Check faults
+// Define CAN Pins
+#define PIN_SPI_CAN_CS 5
+#define PIN_CAN_INT 2
+#define ID_FAULTLATCHER_FAULTS 0X80 // "SEND" CAN Bus address
+MCP_CAN CAN(PIN_SPI_CAN_CS); // set CS Pin for CAN
 
-// Send value over CAN
+// Fault indicator pins
+#define IMD_IND 3
+#define TMS2_IND 4
+#define TMS1_IND 6
+#define BMS_IND 7
+
+// default fault states
+// "1" is faulted, "0" is clear
+uint16_t BMS_FAULT = 1; // BMS
+uint16_t TMS1_FAULT = 1; // Temp for Pack 1
+uint16_t TMS2_FAULT = 1; // Temp for Pack 2
+uint16_t TMS_Fault = TMS1_FAULT | TMS2_FAULT; // TMS fault for either pack 1/2
+uint16_t IMD_FAULT = 1; // IMD
+
+uint16_t lastSendDaqMessage = millis();
+uint16_t DAQ_CAN_INTERVAL = 100; // time in ms
+
+
+void sendDaqData(){
+  // turn off interrupts (interrupts not used, but here for consistancy)
+  cli();
+
+  uint8_t bufToSend[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+  bufToSend[0] = BMS_FAULT;
+  bufToSend[1] = TMS1_FAULT && TMS2_FAULT;
+  bufToSend[2] = IMD_FAULT;
+
+  // send message
+  CAN.sendMsgBuf(ID_FAULTLATCHER_FAULTS, 0, 8, bufToSend);
+
+  // reenable interrupts (interrupts not used, but here for consistency)
+  sei();
+
+  // update last send time stamp
+  lastSendDaqMessage = millis();
+
+}
 
 void setup() {
-  // put your setup code here, to run once:
+  // define pin modes
+  pinMode(BMS_IND, INPUT);
+  pinMode(TMS1_IND, INPUT);
+  pinMode(TMS2_IND, INPUT);
+  pinMode(IMD_IND, INPUT);
+
+  // start CAN interface
+  CAN.begin(MCP_ANY, CAN_500KBPS, MCP_16MHZ);
+  CAN.setMode(MCP_NORMAL);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // if time to send data, sample pins
+  if(millis() > (lastSendDaqMessage + DAQ_CAN_INTERVAL)){
+    BMS_FAULT = digitalRead(BMS_IND);
+    TMS1_FAULT = digitalRead(TMS1_IND);
+    TMS2_FAULT = digitalRead(TMS2_IND);
+    IMD_FAULT = digitalRead(IMD_IND);
+
+    // send message
+    sendDaqData();
+  }
+
 }
