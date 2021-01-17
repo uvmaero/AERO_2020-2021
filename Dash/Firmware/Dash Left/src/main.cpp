ff479cc25283ef1ce374a9e379b2d2d15dbcc3ad
georgespearing <spearingg@gmail.com>
2021-01-16 20:29:43 -0500

    add can setup

--- Dash/Firmware/Dash Left/src/main.cpp
+++ Dash/Firmware/Dash Left/src/main.cpp
@@ -9,6 +9,25 @@
 #include <Arduino.h>
 #include <mcp_can.h>
 
+// CAN Setup
+#define PIN_SPI_CAN_CS 5 // CAN SPI Chip
+#define CAN_INT 2 // interrupt pin
+MCP_CAN CAN(PIN_SPI_CAN_CS); // set CS Pin
+#define DAQ_CAN_INTERVAL 100 // time in ms
+uint16_t lastSendDaqMessage = millis();
+
+// CAN Address
+#define ID_BASE 0x76
+#define ID_DASH_SELF_TEST 0x72 // SAME AS DASH RIGHT
+
+// CAN Address Inputs
+// Battery Temp
+// Motor Temp
+// RineHart Temp
+// Battery Voltage
+
+
+
 // Temperature Sampling
 
 // Battery SOC Calculation
